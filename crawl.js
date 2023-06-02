2e0981c12035f0ce1f9a0b3ac9567574592d831b
juan ramirez arias <jcra.juancamilo@gmail.com>
2023-06-02 10:38:07 -0500

    adding more tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,19 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      urls.push(`${baseURL}${linkElement.href}`);
+    } else {
+      urls.push(linkElement.href);
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlobj = new URL(urlString);
   const hostPath = `${urlobj.hostname}${urlobj.pathname}`;
@@ -10,4 +26,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
